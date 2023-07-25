6dce82ba416c9711474d0926d938f1fd7d79fdb1
Mayssae El Ahmadie <mayssae.ahmadie@gmail.com>
2023-07-25 17:32:55 +0300

    Done with task 3

---
+++ task_3/script.js
@@ -0,0 +1,26 @@
+const num1 = 60;
+var num2 = 70;
+var canPass = true;
+if (canPass == false) {
+    if (num1 >= num2) {
+        console.log("Hello");
+    }
+    else { console.log("Bye"); }
+} else { console.log("Try again"); }
+
+
+var num2 = 70;
+if (num1 >= num2) {
+    console.log("Hello");
+}
+else { console.log("Bye"); }
+
+var num2 = 50;
+if (num1 >= num2) {
+    console.log("Hello");
+}
+else { console.log("Bye"); }
+
+
+
+
